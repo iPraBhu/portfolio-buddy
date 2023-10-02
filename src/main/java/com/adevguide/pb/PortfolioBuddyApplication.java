09f8c24b98c9368f4f3b5a99d6383bddf66a0cd8
Pratik Bhuite <pratik.bhuite@gmail.com>
2023-10-02 16:22:56 -0700

    clearing things up

--- src/main/java/com/adevguide/pb/PortfolioBuddyApplication.java
+++ src/main/java/com/adevguide/pb/PortfolioBuddyApplication.java
@@ -1,13 +1,22 @@
 package com.adevguide.pb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PortfolioBuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBuddyApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+			System.exit(147);
+		};
 	}
 
 }
